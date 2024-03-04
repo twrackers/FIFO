e4c3555521350448d140f4d01b5d4f317def1d97
Thomas W Rackers <twrackers@gmail.com>
2024-03-04 13:18:09 -0500

    Added FIFO_Type_Test example sketch

---
+++ examples/FIFO_Type_Test/FIFO_Type_Test.ino
@@ -0,0 +1,34 @@
+#include <Streaming.h>
+
+#include <FIFO_Type.h>
+
+#define NUMVALS 50
+
+typedef uint16_t data_t;
+
+FIFO_Type<data_t> wfifo(NUMVALS);
+
+void setup() {
+
+  Serial.begin(115200);
+  while (!Serial) delay(10);
+  
+  randomSeed(analogRead(A0));
+  
+}
+
+data_t accum = 0;
+
+void loop() {
+
+  if (wfifo.isFull()) {
+    accum -= wfifo.pop();
+  }
+  data_t newval = (data_t) random(1000);
+  accum += newval;
+  wfifo.push(newval);
+  Serial << newval << " " << (accum / NUMVALS) << endl;
+
+  delay(20);
+  
+}
